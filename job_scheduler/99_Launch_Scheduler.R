de0b1e6ff974dac7bb73111ad73c4f51d6350db8
Angus-Morton <angus.morton@phs.scot>
2025-02-05 16:17:53 +0000

    Change file paths now that we're in the job_scheduler folder

--- job_scheduler/99_Launch_Scheduler.R
+++ job_scheduler/99_Launch_Scheduler.R
@@ -18,16 +18,16 @@ renv::restore(
   prompt = FALSE)
 
 # Define functions to write to stdout and stderr
-source(file.path(here::here(), "code", "01_Functions_00_write_output_to_stdout_or_stderr.R"))
+source(file.path(here::here(), "job_scheduler", "01_Functions_00_write_output_to_stdout_or_stderr.R"))
 
 # Define launch_workbench_job()
-source(file.path(here::here(), "code", "01_Functions_01_launch_workbench_job.R"))
+source(file.path(here::here(), "job_scheduler", "01_Functions_01_launch_workbench_job.R"))
 
 # Define schedule_workbench_job()
-source(file.path(here::here(), "code", "01_Functions_02_schedule_workbench_job.R"))
+source(file.path(here::here(), "job_scheduler", "01_Functions_02_schedule_workbench_job.R"))
 
 # Define cancel_workbench_job_schedule()
-source(file.path(here::here(), "code", "01_Functions_03_cancel_workbench_job_schedule.R"))
+source(file.path(here::here(), "job_scheduler", "01_Functions_03_cancel_workbench_job_schedule.R"))
 
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
